2f3c6ea93f146394adef35c0ebd304e0076b6589
cse-mkamble <cse.mkamble@gmail.com>
2021-07-16 14:39:27 +0530

    make it better

--- src/server.js
+++ src/server.js
@@ -36,12 +36,12 @@ mongoose.connect(URI, {
     console.log('Connected to mongodb')
 })
 
-// if(process.env.NODE_ENV === 'production'){
-//     app.use(express.static('client/build'))
-//     app.get('*', (req, res) => {
-//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-//     })
-// }
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
 
 // Routes
 app.use('/api', require('./routes/auth.routes'))
